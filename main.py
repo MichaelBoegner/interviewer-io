e794150f2568039a73cfb521970a3880718064a1
michaelboegner <cmikeb@gmail.com>
2024-08-13 17:34:06 -0400

    trying to write custom validation for more user friendly error handling but isn't working

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-from typing import List
-from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 app = FastAPI()
 
 class User(BaseModel):
     name: str
-    email: str
-    age: int
+    email: EmailStr
+    age: Optional[int]
 
 class UserResponse(BaseModel):
     name: str
-    email: str
-
-@app.get("/user")
-async def get_hello():
-    user = {
-        "name": "Mike",
-        "email": "cmikeb",
-        "age": "42"
-    }
-    return user
+    email: EmailStr
 
 @app.post("/users/create", response_model=UserResponse)
 async def create_user(user: User):
-    # You can perform any custom processing here
-    if user.age < 18:
-        raise HTTPException(status_code=400, detail="Age must be at least 18")
-    return user
+    # Custom validation logic
+    if not user.name:
+        raise HTTPException(status_code=400, detail="Name must be provided.")
+    if not user.email:
+        raise HTTPException(status_code=400, detail="Email must be provided.")
+    
+    # Process and return response data
+    user_response = {
+        "name": user.name,
+        "email": user.email
+    }
+    
+    return user_response
